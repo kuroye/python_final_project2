e2d4d977d343272904696844d598c3d72fcf0fd4
kuro_ye <erhanaket77@gmail.com>
2022-02-28 23:31:02 +0600

    update: :hammer: add postgresql setting

--- python_final_project2/settings.py
+++ python_final_project2/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'python_final_project2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'image',
+        'USER': 'postgres',
+        'PASSWORD': 'er2003618',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
