f91d6ced67375d6d32d08c16b60006d2281cbc69
kuro_ye <erhanaket77@gmail.com>
2022-02-28 23:31:27 +0600

    new model Image

--- image/models.py
+++ image/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+
+    path = models.CharField(max_length=255)
+    result = models.CharField(max_length=50)
+
