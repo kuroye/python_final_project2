2805128f57c56f33dd590fc3f2c8b8a8edd1df3b
kuro_ye <erhanaket77@gmail.com>
2022-03-02 03:09:32 +0600

    enable attr to null

--- image/models.py
+++ image/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Image(models.Model):
 
     image = models.ImageField(null=True, blank=True)
-    path = models.CharField(max_length=255)
-    result = models.CharField(max_length=50)
+    path = models.CharField(max_length=255, null=True, blank=True)
+    result = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
         db_table = 'image'
